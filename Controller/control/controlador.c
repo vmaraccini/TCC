2b64d3b2032c8bf61a64c4d013ce948e562013b2
Victor Maraccini <vgm.maraccini@gmail.com>
2016-11-09 21:25:49 -0200

    Add semaphores to synchronize control loop

--- Controller/control/controlador.c
+++ Controller/control/controlador.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "controlador.h"
 #include "controller.h"
 #include "controlLoop.h"
-#include "vehicle.h"
+#include "semaphore.h"
+
+extern sem_t cts;
 
 #define INITIAL_VELOCITY 14 / 3.6 //14 km/h is the smallest velocity allowed by 3rd gear
 #define MAX_VELOCITY 60 / 3.6
 
+#define TS 0.1 //Sampling time (seconds)
+#define MICROSECONDS_TO_SECONDS 1E6
+
 void main_controlador() {
     
     for (;;) {
+        usleep(MICROSECONDS_TO_SECONDS * TS);
         
         // car.distance = Distância do carro da frente (m)
         // car.velocity = velocidade atual em relação ao chão (m/s);
@@ -21,6 +28,9 @@ void main_controlador() {
      
         //Report a value from 0 to 255
         pedalByte = (int)(pedal*100) & 0xFF;
+        
+        //Signal that new values are available to broadcast
+        sem_post(&cts);
     }
 }
 
