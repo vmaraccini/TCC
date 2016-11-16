1bc4d968a4e1c784e6fd42b06fbca4a331f1a505
Denis Isidoro <den.isidoro@gmail.com>
2016-11-16 10:22:41 -0200

    Remove server code inside realtime.py

--- opencv-traffic-sign/realtime.py
+++ opencv-traffic-sign/realtime.py
@@ -3,9 +3,6 @@ import recognize
 import process
 import tools
 from copy import deepcopy
-from socketserver import BaseRequestHandler, UDPServer
-import thread
-import time
 
 lastKnownVelocity = None
 def captureAndRecognize(videoCapture):
@@ -23,29 +20,8 @@ def captureAndRecognize(videoCapture):
     display = tools.printRecognizedDigits(frame, lastKnownVelocity)
     cv2.imshow("Capture", display)
 
-class TimeHandler(BaseRequestHandler):
-    def handle(self):
-        print('Got connection from', self.client_address)
-        # Get message and client socket
-        msg, sock = self.request
-        resp = time.ctime()
-        sock.sendto(resp.encode('ascii'), self.client_address)
-
 cap = cv2.VideoCapture(0)
-serv = UDPServer(('', 20000), TimeHandler)
 
 def calculate():	
     while True:
          captureAndRecognize(cap)
-
-def serve():
-    serv.serve_forever()
-
-# Create two threads as follows
-try:
-   thread.start_new_thread(calculate, ())
-   thread.start_new_thread(serve, ())
-except:
-   print "Error: unable to start thread"
-while 1:
-   pass    
