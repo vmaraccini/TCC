897a90be6ee5ba1a639a953008579a73f187f5eb
Denis Isidoro <den.isidoro@gmail.com>
2016-11-15 23:39:29 -0200

    Start implementing server

--- opencv-traffic-sign/realtime.py
+++ opencv-traffic-sign/realtime.py
@@ -1,8 +1,11 @@
 import cv2
 import recognize
 import process
-import  tools
+import tools
 from copy import deepcopy
+from socketserver import BaseRequestHandler, UDPServer
+import thread
+import time
 
 lastKnownVelocity = None
 def captureAndRecognize(videoCapture):
@@ -20,7 +23,29 @@ def captureAndRecognize(videoCapture):
     display = tools.printRecognizedDigits(frame, lastKnownVelocity)
     cv2.imshow("Capture", display)
 
+class TimeHandler(BaseRequestHandler):
+    def handle(self):
+        print('Got connection from', self.client_address)
+        # Get message and client socket
+        msg, sock = self.request
+        resp = time.ctime()
+        sock.sendto(resp.encode('ascii'), self.client_address)
 
 cap = cv2.VideoCapture(0)
-while True:
-    captureAndRecognize(cap)
+serv = UDPServer(('', 20000), TimeHandler)
+
+def calculate():	
+    while True:
+         captureAndRecognize(cap)
+
+def serve():
+    serv.serve_forever()
+
+# Create two threads as follows
+try:
+   thread.start_new_thread(calculate, ())
+   thread.start_new_thread(serve, ())
+except:
+   print "Error: unable to start thread"
+while 1:
+   pass    
