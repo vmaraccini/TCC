2cd4f4851791967acffceba4dcb7b5f0b5eaf423
Victor Maraccini <vgm.maraccini@gmail.com>
2016-11-20 15:44:07 -0200

    Working RS232

--- Controller/rs232/rs232.c
+++ Controller/rs232/rs232.c
@@ -6,7 +6,7 @@
 //  Copyright © 2016 TCC. All rights reserved.
 //
 
-#include "json.h"
+#include "../json/json.h"
 
 #include <unistd.h>
 #include <string.h>
@@ -38,9 +38,9 @@ void sendData() {
 ///Reads 2 bytes off UART and populates currentVelocity
 void readData() {
     if (read(uart_fd, buffer, 2) == 2) {
-        currentVelocity = buffer[0] + (buffer[1] << 8);
+        currentVelocity = buffer[0] + (buffer[1] * 256);
     }
-    
+
 #if DEBUG
     struct timeval temp;
     gettimeofday(&temp, NULL);
@@ -48,7 +48,7 @@ void readData() {
     tv = temp;
     printf("Time between reads: %lu", delta);
 #endif
-    
+
     //Flush after read to discard dangling bytes
     tcflush(uart_fd, TCIOFLUSH);
 }
@@ -56,25 +56,25 @@ void readData() {
 ///Configures UART for communication
 void configureUART(int* fdes, int mode) {
     //Open UART in blocking read/write mode
-    *fdes = open("/dev/ttyUSB0", mode);
+    *fdes = open("/dev/ttyUSB1", mode);
     if (*fdes == -1) {
         //ERROR - CAN'T OPEN SERIAL PORT
         perror("Error - Unable to open UART\n");
     }
-    
+
     //Configure UART
     struct termios options;
     tcgetattr(*fdes, &options);
     cfsetospeed(&options, (speed_t)B9600);
     cfsetispeed(&options, (speed_t)B9600);
-    
+
     options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;
     options.c_cflag &= ~CRTSCTS; //Disable flow control
     options.c_cflag &= ~PARENB; //Disable parity
     options.c_cc[VMIN] = 0; //Pure time read. Wait for VTIME else return 0
-    options.c_cc[VTIME] = 5; //0.5 sec read timeout
+    options.c_cc[VTIME] = 0.1; //0.1 sec read timeout
     options.c_iflag &= ~(IXON | IXOFF | IXANY); //Disable flow control options
-    
+
     tcflush(*fdes, TCIFLUSH);
     tcsetattr(*fdes, TCSANOW, &options);
 }
@@ -83,7 +83,7 @@ void configureUART(int* fdes, int mode) {
 ///@discussion Sends one pedal byte (0-100% from 0-255) and receives 2 velocity bytes from UART.
 void main_rs232() {
     configureUART(&uart_fd, O_RDWR | O_NOCTTY);
-    
+
     for (;;) {
         //Wait for new data
         sem_wait(&cts);
