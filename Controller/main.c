5ddd7f81904d8318fd51d29656cd2d21dd31e872
Victor Maraccini <vgm.maraccini@gmail.com>
2016-11-20 15:44:34 -0200

    Update variables; Remove JSON thread for now

--- Controller/main.c
+++ Controller/main.c
@@ -13,18 +13,19 @@
 #include <stdio.h>
 
 //----------- Variables -----------
+//char accEnabled = 0;
 ///Single byte representing the pedal position (0-100%) Unit: None
-volatile char pedalByte;
+char pedalByte;
 ///Single byte representing the maximum allowed velocity (0-255) Unit: km/h
-volatile char maxVelocity;
+int maxVelocity;
 ///32-bit Integer representing the leader distance (0-2^16) Unit: cm
 ///@discussion: A distance of 2^16 means no detection.
-volatile int leaderDistance;
+int leaderDistance;
 ///32-bit Integer representing the leader velocity. Unit: cm/s
 ///@discussion: A velocity of 2^16 means no detection.
-volatile int leaderVelocity;
+int leaderVelocity;
 ///16-bit Integer representing the vehicle's current velocity (0-2^16) Unit: dm/h
-volatile int currentVelocity;
+int currentVelocity;
 
 //----------- Synchronization variables -----------
 ///Clear to send: Synchronizes control loop with RS232 comm.
@@ -42,15 +43,15 @@ int main(int argc, const char * argv[]) {
     pthread_create(&udpMaxVelocityThread, NULL, (void*) main_udpMaxVelocity, NULL);
     pthread_create(&udpDistanceThread, NULL, (void*) main_udpDistance, NULL);
     pthread_create(&controllerThread, NULL, (void*) main_sampler, NULL);
-    pthread_create(&jsonSaveThread, NULL, (void*) main_json, NULL);
+    //pthread_create(&jsonSaveThread, NULL, (void*) main_json, NULL);
     pthread_create(&rs232Thread, NULL, (void*) main_rs232, NULL);
-    
+
     //Join
     pthread_join(udpMaxVelocityThread, NULL);
     pthread_join(udpDistanceThread, NULL);
     pthread_join(controllerThread, NULL);
-    pthread_join(jsonSaveThread, NULL);
+    //pthread_join(jsonSaveThread, NULL);
     pthread_join(rs232Thread, NULL);
-    
+
     return OK;
 }
